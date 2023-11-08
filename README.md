7dd3cd272e74657409debda27158058b4ee30f46
Ranjith677 <107233964+Ranjith677@users.noreply.github.com>
2023-11-08 06:12:43 +0530

    2

--- README.md
+++
@@ -1,1 +0,0 @@
-# Web-series_Quotes_Generator![Screenshot (278)](https://user-images.githubusercontent.com/106732392/236904046-4e496bcd-07e5-46d6-8db2-2b8df0302168.png)
--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-let quote = document.getElementById("quote");
-let author = document.getElementById("author");
-let btn = document.getElementById("btn");
 
-const URL = "https://web-series-quotes-api.deta.dev/quote/";
+let API_KEY = 'live_T1s6FKlQ3drSWOn04bqnBPlUYlNHpDeRtdE2kOcgHRZhV1L05m9omstFR5v0BDXB'
 
-// const quote_url="https://api.quotable.io/random";
+let cat_api = async () => {
 
-const getQuote = (() => {
-    fetch(URL)
-        .then(res => res.json())
-        .then((item) => {
-            quote.innerHTML = item[0].quote;
-            author.innerHTML = `- ${item[0].series}`;
-        });
+    let response = `https://api.thecatapi.com/v1/images/search?${API_KEY}`
+    let res = await fetch(response)
+    let ele = await res.json()
+    console.log(ele)
+    for (let data of ele) {
+        console.log(data);
+        let parent_div = document.querySelector('.container')
+        //Creating Element and Append
+        let div_tag = document.createElement('div')
+        div_tag.setAttribute('class', 'img_tag')
+        let img_tag = document.createElement('img')
+        img_tag.src = data.url
+        div_tag.append(img_tag)
+        parent_div.append(div_tag)
+
+    }
+}
+cat_api()
+
+let btn1 = document.querySelector('.btn')
+btn1.addEventListener('click', () => {
+    document.location.reload();
 })
-window.addEventListener("load", getQuote)
-btn.addEventListener("click", getQuote);
